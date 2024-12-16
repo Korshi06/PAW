fd3210595e649167f315d8f4db3e6c9055b13306
korshi06 <michalinamichta11@gmail.com>
2024-12-16 13:45:21 +0100

    name change

--- 7.10/routes/params.js
+++ 7.10/routes/params.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
     let queryParams = req.query;
     let json_content = {};
